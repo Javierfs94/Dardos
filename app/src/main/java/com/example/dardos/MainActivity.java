12c189df5ee688536422a3ad9d94769b1fa780d0
Javierfs <javifs94@gmail.com>
2020-05-13 10:08:59 +0200

    Refactorizacion

--- app/src/main/java/com/example/dardos/MainActivity.java
+++ app/src/main/java/com/example/dardos/MainActivity.java
@@ -20,84 +20,60 @@ public class MainActivity extends AppCompatActivity {
     int puntuacionJugador2 = 351;
     int puntuacionJugador3 = 351;
     int puntuacionJugador4 = 351;
-    Button buttonx2;
-    Button buttonx3;
-    Button buttonP1;
-    Button buttonP2;
-    Button buttonP3;
-    Button buttonP4;
+    Button buttons[] = new Button[29];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = (Button) findViewById(R.id.button1);
-        final Button button2 = (Button) findViewById(R.id.button2);
-        final Button button3 = (Button) findViewById(R.id.button3);
-        final Button button4 = (Button) findViewById(R.id.button4);
-        final Button button5 = (Button) findViewById(R.id.button5);
-        final Button button6 = (Button) findViewById(R.id.button6);
-        final Button button7 = (Button) findViewById(R.id.button7);
-        final Button button8 = (Button) findViewById(R.id.button8);
-        final Button button9 = (Button) findViewById(R.id.button9);
-        final Button button10 = (Button) findViewById(R.id.button10);
-        final Button button11 = (Button) findViewById(R.id.button11);
-        final Button button12 = (Button) findViewById(R.id.button12);
-        final Button button13 = (Button) findViewById(R.id.button13);
-        final Button button14 = (Button) findViewById(R.id.button14);
-        final Button button15 = (Button) findViewById(R.id.button15);
-        final Button button16 = (Button) findViewById(R.id.button16);
-        final Button button17 = (Button) findViewById(R.id.button17);
-        final Button button18 = (Button) findViewById(R.id.button18);
-        final Button button19 = (Button) findViewById(R.id.button19);
-        final Button button20 = (Button) findViewById(R.id.button20);
-        final Button button25 = (Button) findViewById(R.id.button25);
-        final Button button50 = (Button) findViewById(R.id.button50);
-        buttonx2 = (Button) findViewById(R.id.buttonx2);
-        buttonx3 = (Button) findViewById(R.id.buttonx3);
-        buttonP1 = (Button) findViewById(R.id.buttonP1);
-        buttonP2 = (Button) findViewById(R.id.buttonP2);
-        buttonP3 = (Button) findViewById(R.id.buttonP3);
-        buttonP4 = (Button) findViewById(R.id.buttonP4);
-        final Button reset = (Button) findViewById(R.id.reset);
+        buttons[0] = (Button) findViewById(R.id.button1);
+        buttons[1] = (Button) findViewById(R.id.button2);
+        buttons[2] = (Button) findViewById(R.id.button3);
+        buttons[3] = (Button) findViewById(R.id.button4);
+        buttons[4] = (Button) findViewById(R.id.button5);
+        buttons[5] = (Button) findViewById(R.id.button6);
+        buttons[6] = (Button) findViewById(R.id.button7);
+        buttons[7] = (Button) findViewById(R.id.button8);
+        buttons[8] = (Button) findViewById(R.id.button9);
+        buttons[9] = (Button) findViewById(R.id.button10);
+        buttons[10] = (Button) findViewById(R.id.button11);
+        buttons[11] = (Button) findViewById(R.id.button12);
+        buttons[12] = (Button) findViewById(R.id.button13);
+        buttons[13] = (Button) findViewById(R.id.button14);
+        buttons[14] = (Button) findViewById(R.id.button15);
+        buttons[15] = (Button) findViewById(R.id.button16);
+        buttons[16] = (Button) findViewById(R.id.button17);
+        buttons[17] = (Button) findViewById(R.id.button18);
+        buttons[18] = (Button) findViewById(R.id.button19);
+        buttons[19] = (Button) findViewById(R.id.button20);
+        buttons[20] = (Button) findViewById(R.id.button25);
+        buttons[21] = (Button) findViewById(R.id.button50);
+        buttons[22] = (Button) findViewById(R.id.buttonx2);
+        buttons[23] = (Button) findViewById(R.id.buttonx3);
+        buttons[24] = (Button) findViewById(R.id.buttonP1);
+        buttons[25] = (Button) findViewById(R.id.buttonP2);
+        buttons[26] = (Button) findViewById(R.id.buttonP3);
+        buttons[27] = (Button) findViewById(R.id.buttonP4);
+        buttons[28] = (Button) findViewById(R.id.reset);
 
-        buttonP1.setOnClickListener(cambiarJugador);
-        buttonP2.setOnClickListener(cambiarJugador);
-        buttonP3.setOnClickListener(cambiarJugador);
-        buttonP4.setOnClickListener(cambiarJugador);
+        buttons[24].setOnClickListener(cambiarJugador);
+        buttons[25].setOnClickListener(cambiarJugador);
+        buttons[26].setOnClickListener(cambiarJugador);
+        buttons[27].setOnClickListener(cambiarJugador);
 
-        button1.setOnClickListener(calcularPuntos);
-        button2.setOnClickListener(calcularPuntos);
-        button3.setOnClickListener(calcularPuntos);
-        button4.setOnClickListener(calcularPuntos);
-        button5.setOnClickListener(calcularPuntos);
-        button6.setOnClickListener(calcularPuntos);
-        button7.setOnClickListener(calcularPuntos);
-        button8.setOnClickListener(calcularPuntos);
-        button9.setOnClickListener(calcularPuntos);
-        button10.setOnClickListener(calcularPuntos);
-        button11.setOnClickListener(calcularPuntos);
-        button12.setOnClickListener(calcularPuntos);
-        button13.setOnClickListener(calcularPuntos);
-        button14.setOnClickListener(calcularPuntos);
-        button15.setOnClickListener(calcularPuntos);
-        button16.setOnClickListener(calcularPuntos);
-        button17.setOnClickListener(calcularPuntos);
-        button18.setOnClickListener(calcularPuntos);
-        button19.setOnClickListener(calcularPuntos);
-        button20.setOnClickListener(calcularPuntos);
-        button25.setOnClickListener(calcularPuntos);
-        button50.setOnClickListener(calcularPuntos);
-        buttonx2.setOnClickListener(multiplicarPuntos);
-        buttonx3.setOnClickListener(multiplicarPuntos);
-        reset.setOnClickListener(limpiar);
+        for (int i = 0; i < 22; i++) {
+            buttons[i].setOnClickListener(calcularPuntos);
+        }
 
-        buttonP1.setBackgroundColor(Color.GREEN);
+        buttons[22].setOnClickListener(multiplicarPuntos);
+        buttons[23].setOnClickListener(multiplicarPuntos);
+        buttons[28].setOnClickListener(limpiar);
+
+        buttons[24].setBackgroundColor(Color.GREEN);
 
         textPuntos = (EditText) findViewById(R.id.puntos);
         textPuntos.setEnabled(false);
-
     }
 
     private View.OnClickListener calcularPuntos = new View.OnClickListener() {
@@ -177,11 +153,11 @@ public class MainActivity extends AppCompatActivity {
             if (doble){
                 puntos = puntos * 2;
                 doble = false;
-                buttonx2.setBackgroundColor(Color.LTGRAY);
+                buttons[22].setBackgroundColor(Color.LTGRAY);
             } else if (triple){
                 puntos = puntos * 3;
                 triple = false;
-                buttonx3.setBackgroundColor(Color.LTGRAY);
+                buttons[23].setBackgroundColor(Color.LTGRAY);
             }
 
             if (jugadorActivo == 1){
@@ -211,29 +187,24 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void lanzar() {
-        Intent i = new Intent(this, Victory.class );
-        startActivity(i);
+        startActivity(new Intent(this, Victory.class));
     }
-
 
     private View.OnClickListener multiplicarPuntos = new View.OnClickListener() {
        @Override
         public void onClick(View v) {
-            puntuacion =  Integer.parseInt(textPuntos.getText().toString());
-            int puntos = 0;
             switch(v.getId()){
                 case R.id.buttonx2:
                     doble = true;
-                    buttonx2.setBackgroundColor(Color.RED);
+                    buttons[22].setBackgroundColor(Color.RED);
                     break;
                 case R.id.buttonx3:
                     triple = true;
-                    buttonx3.setBackgroundColor(Color.RED);
+                    buttons[23].setBackgroundColor(Color.RED);
                     break;
             }
         }
     };
-
 
     private View.OnClickListener cambiarJugador = new View.OnClickListener() {
         @Override
@@ -242,34 +213,34 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.buttonP1:
                     jugadorActivo = 1;
                     textPuntos.setText(String.valueOf(puntuacionJugador1));
-                    buttonP1.setBackgroundColor(Color.GREEN);
-                    buttonP2.setBackgroundColor(Color.LTGRAY);
-                    buttonP3.setBackgroundColor(Color.LTGRAY);
-                    buttonP4.setBackgroundColor(Color.LTGRAY);
+                    buttons[24].setBackgroundColor(Color.GREEN);
+                    buttons[25].setBackgroundColor(Color.LTGRAY);
+                    buttons[26].setBackgroundColor(Color.LTGRAY);
+                    buttons[27].setBackgroundColor(Color.LTGRAY);
                     break;
                 case R.id.buttonP2:
                     jugadorActivo = 2;
                     textPuntos.setText(String.valueOf(puntuacionJugador2));
-                    buttonP1.setBackgroundColor(Color.LTGRAY);
-                    buttonP2.setBackgroundColor(Color.GREEN);
-                    buttonP3.setBackgroundColor(Color.LTGRAY);
-                    buttonP4.setBackgroundColor(Color.LTGRAY);
+                    buttons[24].setBackgroundColor(Color.LTGRAY);
+                    buttons[25].setBackgroundColor(Color.GREEN);
+                    buttons[26].setBackgroundColor(Color.LTGRAY);
+                    buttons[27].setBackgroundColor(Color.LTGRAY);
                     break;
                 case R.id.buttonP3:
                     jugadorActivo = 3;
                     textPuntos.setText(String.valueOf(puntuacionJugador3));
-                    buttonP1.setBackgroundColor(Color.LTGRAY);
-                    buttonP2.setBackgroundColor(Color.LTGRAY);
-                    buttonP3.setBackgroundColor(Color.GREEN);
-                    buttonP4.setBackgroundColor(Color.LTGRAY);
+                    buttons[24].setBackgroundColor(Color.LTGRAY);
+                    buttons[25].setBackgroundColor(Color.LTGRAY);
+                    buttons[26].setBackgroundColor(Color.GREEN);
+                    buttons[27].setBackgroundColor(Color.LTGRAY);
                     break;
                 case R.id.buttonP4:
                     jugadorActivo = 4;
                     textPuntos.setText(String.valueOf(puntuacionJugador4));
-                    buttonP1.setBackgroundColor(Color.LTGRAY);
-                    buttonP2.setBackgroundColor(Color.LTGRAY);
-                    buttonP3.setBackgroundColor(Color.LTGRAY);
-                    buttonP4.setBackgroundColor(Color.GREEN);
+                    buttons[24].setBackgroundColor(Color.LTGRAY);
+                    buttons[25].setBackgroundColor(Color.LTGRAY);
+                    buttons[26].setBackgroundColor(Color.LTGRAY);
+                    buttons[27].setBackgroundColor(Color.GREEN);
                     break;
             }
         }
@@ -288,7 +259,6 @@ public class MainActivity extends AppCompatActivity {
             textPuntos.setText(String.valueOf(puntuacionJugador4));
         }
     };
-
 }
 
 
